8676f514036d34d6941f97e893802314cd6927a5
bbuchsbaum <brad.buchsbaum@gmail.com>
2021-06-15 16:36:19 -0400

    wrap templateflow in try

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,12 @@ tflow <<- NULL
 .onLoad <- function(libname, pkgname) {
   # use superassignment to update global reference to scipy
   #tflow <<- reticulate::import("scipy", delay_load = TRUE)
-  tflow <<- reticulate::import("templateflow", delay_load=TRUE)
+  tflow <<- try(reticulate::import("templateflow", delay_load=TRUE))
+  
 }
 
 #' @export
 install_templateflow <- function(method = "auto", conda = "auto") {
   reticulate::py_install("scipy", method = method, conda = conda, pip=TRUE)
+  reticulate::py_install("templateflow", method = method, conda = conda, pip=TRUE)
 }
